ffa9a91b6f392503ef0ae192c8b4276ecc2b7762
nehakalegit <nehakaleofficial@gmail.com>
2026-04-10 17:57:31 +0530

    Update main.tf

--- ci-cd-pipeline-project/terraform/main.tf
+++ ci-cd-pipeline-project/terraform/main.tf
@@ -2,13 +2,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Security Group
+resource "aws_security_group" "ssh_sg" {
+  name        = "ssh-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # for testing only
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   key_name      = "keypairvirginia"
 
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
   tags = {
-    Name = "CI-CD-Server"
+    Name = "Jenkins-ci/cd"
   }
 }
 
