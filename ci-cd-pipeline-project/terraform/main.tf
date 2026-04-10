2609a96c8cc02f40db0c2cef68727bf9d4426fa4
nehakalegit <nehakaleofficial@gmail.com>
2026-04-10 14:15:51 +0530

    Update key_name for AWS instance in main.tf

--- ci-cd-pipeline-project/terraform/main.tf
+++ ci-cd-pipeline-project/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  key_name      = "your-key"
+  key_name      = "keypairvirginia.pem"
 
   tags = {
     Name = "CI-CD-Server"
