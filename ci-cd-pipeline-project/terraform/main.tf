318e681363fe22f2832c8745eb5b18107f859ce9
nehakalegit <nehakaleofficial@gmail.com>
2026-04-10 14:10:14 +0530

    Create main.tf

---
+++ ci-cd-pipeline-project/terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = "your-key"
+
+  tags = {
+    Name = "CI-CD-Server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
