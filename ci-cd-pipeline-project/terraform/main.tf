081df400b1abfb6f8aafaed47db7f5ca88497334
nehakalegit <nehakaleofficial@gmail.com>
2026-04-17 17:08:19 +0530

    Update main.tf

--- ci-cd-pipeline-project/terraform/main.tf
+++ ci-cd-pipeline-project/terraform/main.tf
@@ -24,13 +24,6 @@ resource "aws_security_group" "ssh_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-# Jenkins
-ingress {
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
 
 # SonarQube
 ingress {
